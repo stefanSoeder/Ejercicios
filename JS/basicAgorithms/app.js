9af1b3d551a0d74c0a8ecdb51af7e28da6313641
stefanSoeder <you@example.com>
2024-01-11 17:31:28 +0100

    Iteración#4: Arrays

--- JS/basicAgorithms/app.js
+++ JS/basicAgorithms/app.js
@@ -214,3 +214,58 @@ const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lap
 //!--------------------------------------------------------------------------------------------
 
 const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+console.log(avengers[0]);
+
+//!--------------------------------------------------------------------------------------------
+
+//? 4.2 Cambia el primer elemento de avengers a "IRONMAN"
+//? const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+//!--------------------------------------------------------------------------------------------
+
+avengers[0] = "IRONMAN";
+console.log(avengers);
+
+//!--------------------------------------------------------------------------------------------
+
+//? 4.3 console numero de elementos en el array usando la propiedad correcta de Array.
+//? const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+//!--------------------------------------------------------------------------------------------
+
+console.log(avengers.length);
+
+//!--------------------------------------------------------------------------------------------
+
+//? 4.4 Añade 2 elementos al array: "Morty" y "Summer".
+//? Muestra en consola el último personaje del array
+//? const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+
+//!--------------------------------------------------------------------------------------------
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty", "Summer");
+console.log(rickAndMortyCharacters);
+
+//!--------------------------------------------------------------------------------------------
+
+//? 4.5 Elimina el último elemento del array y muestra el primero y el último por consola.
+//? const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+//!--------------------------------------------------------------------------------------------
+
+rickAndMortyCharacters.push("Lapiz Lopez");
+rickAndMortyCharacters.pop();
+console.log(rickAndMortyCharacters);
+
+//!--------------------------------------------------------------------------------------------
+
+//? 4.6 Elimina el segundo elemento del array y muestra el array por consola.
+//? const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"]; */
+
+//!--------------------------------------------------------------------------------------------
+
+rickAndMortyCharacters.push("Lapiz Lopez");
+rickAndMortyCharacters.splice(1, 1);
+console.log(rickAndMortyCharacters);
---
+++ JS/basicAgorithms/test.js
@@ -0,0 +1,2 @@
+let h = 25;
+console.log(h);
