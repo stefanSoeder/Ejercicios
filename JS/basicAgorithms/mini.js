5be166a2ee0acbffcac6cd0e401eb9ea227fc300
stefanSoeder <you@example.com>
2024-01-14 14:13:25 +0100

    miniEjereciciosCondicionales

--- JS/basicAgorithms/mini.js
+++ JS/basicAgorithms/mini.js
@@ -28,3 +28,23 @@ let isInnerPlanet = true;
 let hasAtmosphere = true;
 isHabitable = isInnerPlanet && hasAtmosphere;
 console.log(isHabitable);
+
+/**Crea una variable llamada **`character`** y asígnale el valor "Legolas"**
+2. Crea una variable llamada **`race`** y asígnale el valor "Elfo"
+3. Crea una variable llamada **`hasRing`** y asígnale el valor **`false`** (Legolas no tiene el Anillo Único)
+4. Crea una variable llamada **`isArcher`** y asígnale el valor **`true`** (Legolas es un arquero)
+5. Utiliza un condicional **`if`** para evaluar si **`hasRing`** es **`true`**. Si lo es, imprime en la consola el mensaje "Legolas es el portador del Anillo Único"
+6. Utiliza **`else if`** para evaluar si **`isArcher`** es **`true`**. Si lo es, imprime en la consola el mensaje "Legolas es un arquero experto"
+7. Utiliza  **`else`** para imprimir en la consola el mensaje "Legolas es un guerrero valiente" */
+
+let character1 = "Legolas";
+let race = "Elfo";
+let hasRing = false;
+let isArcher = true;
+if (hasRing === true) {
+  console.log("Legolas es el portador del Anillo Único");
+} else if ((isArcher = true)) {
+  console.log(character1 + " es un arquero experto");
+} else {
+  console.log("Legolas es un guerrero valiente");
+}
