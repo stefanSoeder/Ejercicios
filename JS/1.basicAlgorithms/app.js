0aa734eb852c940cad486f488b55ab09d90dc3c9
stefanSoeder <156074211+stefanSoeder@users.noreply.github.com>
2024-03-18 13:38:37 +0100

    basicAlgorithms1-5

---
+++ JS/1.basicAlgorithms/app.js
@@ -0,0 +1,123 @@
+//!------------------------
+//? Iteración #1: Variables
+//!------------------------
+// 1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+// 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10.
+// 1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
+let x = 50;
+let h = 5;
+let y = 10;
+let z = h + y;
+console.log(z);
+
+//!---------------------------------
+//?Iteración #2: Variables avanzadas
+//!---------------------------------
+
+// 1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
+const character = { name: "Jack Sparrow", age: 10 };
+character.age = 25;
+console.log(character);
+
+// 1.2 Declara 3 variables con los nombres y valores siguientes
+firstName = "Jon";
+lastName = "Snow";
+age = 24;
+// Muestralos por consola de esta forma:
+// 	'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
+
+console.log(
+  `Soy ${firstName + " " + lastName}, tengo ${age} años y me gustan los lobos`
+);
+
+// 1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
+// ambos juguetes.
+const toy1 = { name: "Buss myYear", price: 19 };
+const toy2 = { name: "Rallo mcKing", price: 29 };
+
+console.log("Total price", toy1.price + toy2.price);
+
+// 1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000
+// y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad
+// basePrice más el valor de la variable globalBasePrice.
+let globalBasePrice = 10000;
+const car1 = { name: "BMW m&m", basePrice: 50000, finalPrice: 60000 };
+const car2 = { name: "Chevrolet Corbina", basePrice: 70000, finalPrice: 80000 };
+globalBasePrice = 25000;
+car1.finalPrice = car1.basePrice + globalBasePrice;
+car2.finalPrice = car2.basePrice + globalBasePrice;
+console.log(
+  "Precio coche 1",
+  car1.finalPrice,
+  "Precio coche 2",
+  car2.finalPrice
+);
+
+//!-------------------------
+//? Iteración #3: Operadores
+//!-------------------------
+
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+let multiplicar = 10 * 5;
+console.log(" Multiplicar", multiplicar);
+
+// 1.2 Divide 10 por 2 y muestra el resultado en un console.
+let dividir = 10 / 2;
+console.log("dividir", dividir);
+
+// 1.3 Muestra mediante un console el resto de dividir 15 por 9.
+let resto = 15 / 9;
+console.log("resto", resto);
+
+// 1.4 Usa el correcto operador de asignación que resultará en o = 15,
+// teniendo dos variables p = 10 y j = 5.
+let p = 10;
+let j = 5;
+let o = p + j;
+console.log("operador correcto", o);
+
+// 1.5 Usa el correcto operador de asignación que resultará en i = 50,
+// teniendo dos variables c = 10 y m = 5.
+let c = 10;
+let m = 5;
+let i = c * m;
+console.log("otro operador", i);
+
+//!---------------------
+//? Iteración #4: Arrays
+//!---------------------
+
+// 1.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+const firstAvenger = avengers[0];
+
+// 1.2 Cambia el primer elemento de avengers a "IRONMAN"
+// const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+avengers[0] = "IRONMAN";
+console.log(avengers);
+// 1.3 console numero de elementos en el array usando la propiedad correcta de Array.
+// const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log("núumero de heroes", avengers.length);
+
+// 1.4 Añade 2 elementos al array: "Morty" y "Summer".
+// Muestra en consola el último personaje del array
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty", "Summer");
+console.log(
+  "añade M y S",
+  rickAndMortyCharacters[rickAndMortyCharacters.length - 1]
+);
+
+// 1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
+// const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters.pop();
+
+console.log(
+  "Primero y último",
+  rickAndMortyCharacters[0] + " ",
+  +rickAndMortyCharacters[rickAndMortyCharacters.length - 1]
+);
+// 1.6 Elimina el segundo elemento del array y muestra el array por consola.
+// const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters.splice(1, 1);
+console.log("Spliced R&M", rickAndMortyCharacters);
