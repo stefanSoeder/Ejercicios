726f3a72e8002b360c54d59def8329c0605fa92d
stefanSoeder <stefanms3@gmail.com>
2024-05-26 12:05:16 +0400

    jsBasicCond

--- JS/1.basicAlgorithms/app.js
+++ JS/1.basicAlgorithms/app.js
@@ -148,11 +148,50 @@ console.log("🚀 ~ rickAndMortyCharactersThree:", rickAndMortyCharactersThree);
 
 // En base al código siguiente, muestra los mensajes correctos por consola.
 
+const number1 = 10;
+const number2 = 20;
+const number3 = 2;
+
+// ejemplo
+if (number1 === 10) {
+  console.log("number1 es estrictamente igual a 10");
+}
+
+if (number2 / number1 == 2) {
+  console.log("number2 dividido entre number1 es igual a 2");
+}
+
+if (number1 !== number2) {
+  console.log("number1 es estrictamente distinto a number2");
+}
+
+if (number3 != number1) {
+  console.log("number3 es distinto number1");
+}
+
+if (number3 * 5 == number1) {
+  console.log("number3 por 5 es igual a number1");
+}
+
+if (number3 * 5 == number1 && number1 * 2 == number2) {
+  console.log(
+    "number3 por 5 es igual a number1 Y number1 por 2 es igual a number2"
+  );
+}
+
+if (number2 == number1 || number1 / 5 == number3) {
+  console.log(
+    "number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3"
+  );
+}
+
 //! --------------------
 //? Iteración #6: Bucles
 //!---------------------
 
 // 1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
+
+//const loopList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // 1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo
 // cuando el resto del numero dividido entre 2 sea 0.
