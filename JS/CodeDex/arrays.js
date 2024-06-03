c37b9524329c24b0afc2a17bb4173c6347e173a2
stefanSoeder <stefanms3@gmail.com>
2024-06-03 18:40:20 +0400

    codeDexArraysTimesMusic

---
+++ JS/CodeDex/arrays.js
@@ -0,0 +1,53 @@
+const myNYCDestinations = [
+  "🗽 Manhattan - Washington Square Park",
+  "🏟️ The Bronx - Yankee Stadium",
+  "🎡 Brooklyn - Coney Island",
+  "✈️ Queens - Astoria Park",
+  "🌉 Staten Island - Historic Richmond Town",
+];
+
+console.log(myNYCDestinations);
+
+// Grocery Trip 🛒
+
+let groceryList = ["🥛 Milk", "🥑 Avocado", "🥚 Eggs", "🍞 Bread"];
+groceryList.splice(2, 1, "🧈 Butter");
+console.log("🚀 ~ groceryList:", groceryList);
+
+// Times Tables ❌
+
+let tablaDel = Math.floor(Math.random() * 10 + 1);
+let multiplicador = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const timesTable = (a, b) => {
+  let score = [];
+  for (let i = 0; i < b.length; i++) {
+    score.push(b[i] * a);
+  }
+  return score;
+};
+
+const tabla = timesTable(tablaDel, multiplicador);
+console.log("🚀 ~ multiplicador:", multiplicador);
+console.log("🚀 ~ tablaDel:", tablaDel);
+console.log("🚀 ~ tabla:", tabla);
+
+// Music Playlist 🎶
+
+const musicPlaylist = [
+  "Tom Sawyer",
+  "Sabotage",
+  "I Wanna Dance With Somebody",
+  "Don't Speak",
+  "Bulls On Parade",
+  "Thriller",
+  "The Breaks",
+  "Brick",
+  "Aeroplane Over the Sea",
+  "Tubthumping",
+];
+
+const shiftedMusic = musicPlaylist.shift();
+console.log("🚀 ~ poppedMusic:", shiftedMusic);
+const poppedMusic = musicPlaylist.pop();
+console.log("🚀 ~ shiftedMusic:", poppedMusic);
+console.log(musicPlaylist);
