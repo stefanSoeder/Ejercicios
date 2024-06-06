8e3ac974d9445e53f167b909502ebfcad026aea8
stefanSoeder <stefanms3@gmail.com>
2024-06-06 20:03:15 +0400

    es6MapFilter

--- JS/es6/app.js
+++ JS/es6/app.js
@@ -125,6 +125,147 @@ const eliminar = (array, index) => {
 const lessColors = [...colors];
 console.log(eliminar(lessColors, 2));
 
+//!----------------
+//?Iteración 4: Map
+//!----------------
+
+// 4.1 Dado el siguiente array, devuelve un array con sus nombres
+// utilizando .map().
+const users = [
+  { id: 1, name: "Abel" },
+  { id: 2, name: "Julia" },
+  { id: 3, name: "Pedro" },
+  { id: 4, name: "Amanda" },
+];
+const usersNames = users.map((users) => users.name);
+console.log("🚀 ~ usersNames:", usersNames);
+
+// 4.2 Dado el siguiente array, devuelve una lista que contenga los valores
+// de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que
+// empiece por 'A'.
+
+let anacletos = [];
+
+usersNames.forEach((element) => {
+  if (element.toLowerCase().charAt(0) == "a") {
+    anacletos.push("Anacleto");
+  } else {
+    anacletos.push(element);
+  }
+});
+console.log("🚀 ~ anacletos:", anacletos);
+
+// 4.3 Dado el siguiente array, devuelve una lista que contenga los valores
+// de la propiedad .name y añade al valor de .name el string ' (Visitado)'
+// cuando el valor de la propiedad isVisited = true.
+
+const cities = [
+  { isVisited: true, name: "Tokyo" },
+  { isVisited: false, name: "Madagascar" },
+  { isVisited: true, name: "Amsterdam" },
+  { isVisited: false, name: "Seul" },
+];
+
+const visitedCities = cities.map((cities) => cities.name);
+console.log("🚀 ~ visitedCities:", visitedCities);
+
+const reVisited = cities.map((cities) => cities.isVisited);
+console.log("🚀 ~ reVisited:", reVisited);
+
+reVisited.forEach((element, index) => {
+  if (element == true) {
+    visitedCities[index] += "/Visitado";
+  }
+});
+console.log(visitedCities);
+
+//!-------------------
+//?Iteración 4: Filter
+//!-------------------
+
+// 5.1 Dado el siguiente array, utiliza .filter() para generar un nuevo array
+// con los valores que sean mayor que 18
+
+const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+const over18 = ages.filter((age) => age > 18);
+console.log("🚀 ~ over18:", over18);
+
+// 5.2 Dado el siguiente array, utiliza .filter() para generar un nuevo array
+// con los valores que sean par.
+
+const even = ages.filter((age) => age % 2 == 0);
+console.log("🚀 ~ even:", even);
+
+// 5.3 Dado el siguiente array, utiliza .filter() para generar un nuevo array
+// con los streamers que tengan el gameMorePlayed = 'League of Legends'.
+
+const streamers = [
+  { name: "Rubius", age: 32, gameMorePlayed: "Minecraft" },
+  { name: "Ibai", age: 25, gameMorePlayed: "League of Legends" },
+  { name: "Reven", age: 43, gameMorePlayed: "League of Legends" },
+  { name: "AuronPlay", age: 33, gameMorePlayed: "Among Us" },
+];
+
+const lOl = streamers.filter(
+  (streamer) => streamer.gameMorePlayed === "League of Legends"
+);
+console.log("🚀 ~ lOl:", lOl);
+
+// 5.4 Dado el siguiente array, utiliza .filter() para generar un nuevo array
+// con los streamers que incluyan el caracter 'u' en su propiedad .name. Recomendamos
+// usar la funcion .includes() para la comprobación.
+
+const includesU = streamers.filter((streamer) => streamer.name.includes("u"));
+console.log("🚀 ~ includesU:", includesU);
+
+// 5.5 utiliza .filter() para generar un nuevo array con los streamers que incluyan
+// el caracter 'Legends' en su propiedad .gameMorePlayed. Recomendamos usar la funcion
+// .includes() para la comprobación.
+// Además, pon el valor de la propiedad .gameMorePlayed a MAYUSCULAS cuando
+// .age sea mayor que 35.
+
+// const lOl35 = streamers.filter((streamer) => {
+//   if (streamer.gameMorePlayed.includes("Legends")) {
+//     if (streamer.age > 35) {
+//       streamer.gameMorePlayed = streamer.gameMorePlayed.toUpperCase();
+//     }
+//   }
+//   return streamer;
+// });
+// console.log("lol35", lOl35);
+
+const streamersFilter2 = streamers.filter((gamer) => {
+  if (gamer.gameMorePlayed.includes("Legends")) {
+    if (gamer.age > 35) {
+      gamer.gameMorePlayed = gamer.gameMorePlayed.toUpperCase();
+    }
+    return gamer;
+  }
+});
+
+console.log(streamersFilter2);
+
+// 5.6 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola
+// los streamers que incluyan la palabra introducida en el input. De esta forma, si
+// introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si
+// introduzco 'i', me deberia de mostrar el streamer 'Rubius' e 'Ibai'.
+
+// <!doctype html>
+// <html lang="en">
+// <head>
+// <meta charset="UTF-8">
+//              <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+//              <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//              <title>Document</title>
+// </head>
+// <body>
+//   <input type="text" data-function="toFilterStreamers"/>
+// </body>
+// </html>
+
+//!---------------
+//? Iteracio
+
 // 6.3 Dado el siguiente javascript, usa .find() para econtrar el alien de nombre
 // 'Cucushumushu' y la mutación 'Porompompero'. Una vez que los encuentres, usa
 // spread operator para fusionarlos teniendo en cuenta que el objeto de la mutación
