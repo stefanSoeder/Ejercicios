bca7217e41bbca5a9fdc9b2dd74a310f61beaf0e
stefanSoeder <stefanms3@gmail.com>
2024-06-28 16:52:46 +0400

    asynchronnous1.1

---
+++ JS/Asynchronous/app.js
@@ -0,0 +1,22 @@
+//!-------------------
+//? Iteración 1: Fetch
+//!-------------------
+
+// 1.1 Utiliza esta url de la api Agify 'https://api.agify.io?name=michael' para
+// hacer un .fetch() y recibir los datos que devuelve. Imprimelo mediante un
+// console.log(). Para ello, es necesario que crees un .html y un .js.
+
+fetch(`https://api.agify.io?name=michael`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+/*1.2 Dado el siguiente javascript y html. Añade la funcionalidad necesaria usando 
+fetch() para hacer una consulta a la api cuando se haga click en el botón, 
+pasando como parametro de la api, el valor del input.*/
