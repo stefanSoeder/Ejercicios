2054cfc96b93575dfe599e45d10a770a7f4b9f93
stefanSoeder <stefanms3@gmail.com>
2024-07-15 11:51:54 +0200

    retoBraisAnagrama

--- retosBrais/app.js
+++ retosBrais/app.js
@@ -20,3 +20,36 @@ for (let i = 1; i <= 100; i++)
   } else {
     console.log(i);
   }
+
+//!----------
+//? FIZZ BUZZ
+//!----------
+
+/*
+ * Escribe una función que reciba dos palabras (String) y retorne
+ * verdadero o falso (Bool) según sean o no anagramas.
+ * - Un Anagrama consiste en formar una palabra reordenando TODAS
+ *   las letras de otra palabra inicial.
+ * - NO hace falta comprobar que ambas palabras existan.
+ * - Dos palabras exactamente iguales no son anagrama.
+ */
+
+const anagramDetector = (string1, string2) => {
+  let isAnagram = true;
+  let string1Array = string1.split("").sort().join("");
+  console.log("🚀 ~ anagramDetector ~ string1Array:", string1Array);
+
+  let string2Array = string2.split("").sort().join("");
+  console.log("🚀 ~ anagramDetector ~ string2Array:", string2Array);
+  string1.split;
+  if (string1 === string2) {
+    isAnagram = false;
+  } else if (string1Array === string2Array) {
+    isAnagram = true;
+  } else {
+    isAnagram = false;
+  }
+  return isAnagram;
+};
+areThisAnagram = anagramDetector("listen", "silent");
+console.log("🚀 ~ areThisAnagram:", areThisAnagram);
